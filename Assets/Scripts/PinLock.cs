3a2bd4b74703f9a037f3c9b57f13c68a8f906be3
IwakiEisuke <get10.ref11.del12@gmail.com>
2024-08-28 16:27:05 +0900

    鍵側のピンを表示上追加

--- Assets/Scripts/PinLock.cs
+++ Assets/Scripts/PinLock.cs
@@ -1,7 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
+using UnityEngine.UIElements;
 
+/// <summary>
+/// 長さがランダムなピンがスクロールし続けるので、それにタイミングを合わせて反対側のピンをがっちりはめると解ける鍵。
+/// </summary>
 public class PinLock : MonoBehaviour
 {
     [SerializeField] int count, maxLength, minLength, screenWidth, screenHeight;
@@ -15,47 +20,57 @@ public class PinLock : MonoBehaviour
 
     void Start()
     {
+        void CreatePin(int Length, int i, float right, out GameObject pin)
+        {
+            var pinSize = Length * wGap;
+
+            pin = Instantiate(pinPref, transform);
+            pin.transform.localScale = new Vector3(pinSize, hGap);
+
+            pin.transform.Translate(0, -(i + 0.5f) * hGap, 0); // 縦に並べる
+            pin.transform.Translate((pinSize - screenWidth) / 2f * right - screenWidth / 2f, 0, 0); // 左右ソート
+            //pin.transform.Translate(0, 0, 0); //上下ソート
+        }
+
+        //錠側のピンを作成する
         pinsLength = new int[count];
         pins = new GameObject[count];
         for (int i = 0; i < count; i++)
         {
-            var thisPinLength = Random.Range(minLength, maxLength + 1);
-            var xSize = thisPinLength * wGap;
-
-            var pin = Instantiate(pinPref, transform);
-            pin.transform.localScale = new Vector3(xSize, hGap);
-            pin.transform.Translate(-xSize / 2f, -(i + 0.5f) * hGap, 0);
-
-            pinsLength[i] = thisPinLength;
-            pins[i] = pin;
+            pinsLength[i] = Random.Range(minLength, maxLength + 1);
+            CreatePin(pinsLength[i], i, -1, out pins[i]);
         }
 
+        // 鍵側のピンを作成する。pinsLengthから長さを範囲で持ってきてmaxLengthとの差を求める
         var start = Random.Range(0, pinsLength.Length);
-        var keyWidth = new int[verifyPinsCount];
+        var keysLength = new int[verifyPinsCount];
+        var keys = new GameObject[verifyPinsCount];
         for (int i = 0; i < verifyPinsCount; i++)
         {
-            keyWidth[i] = pinsLength[(i + start) % pinsLength.Length];
+            keysLength[i] = maxLength - pinsLength[(i + start) % pinsLength.Length];
+            CreatePin(keysLength[i] + 1, i, 1, out keys[i]);
         }
 
-        Debug.Log(string.Join(' ', keyWidth));
+        Debug.Log(string.Join(' ', keysLength));
     }
 
     void Update()
     {
         mask.transform.localScale = new Vector3(screenWidth, screenHeight, 1);
 
-        foreach(var card in pins)
+        // ピンをスクロールさせる
+        foreach(var pin in pins)
         {
-            card.transform.Translate(0, -scrollSpeed * Time.deltaTime, 0);
+            pin.transform.Translate(0, -scrollSpeed * Time.deltaTime, 0);
 
-            if (card.transform.localPosition.y < -screenHeight - hGap / 2)
+            if (pin.transform.localPosition.y < -screenHeight - hGap / 2) // ピンが範囲外に出たら上に戻す
             {
-                card.transform.Translate(0, pins.Length * hGap, 0);
-                card.SetActive(false);
+                pin.transform.Translate(0, pins.Length * hGap, 0);
+                pin.SetActive(false); // 非表示にする
             }
-            else if (card.transform.localPosition.y < hGap / 2)
+            else if (pin.transform.localPosition.y < hGap / 2) // ピンが範囲内に入ったら
             {
-                card.SetActive(true);
+                pin.SetActive(true); // 表示する
             }
         }
     }
