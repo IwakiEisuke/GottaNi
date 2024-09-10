ed25328c42173f5079ae0f1372ce60f748cf3659
IwakiEisuke <get10.ref11.del12@gmail.com>
2024-09-10 11:20:55 +0900

    リザルト画面ブラッシュアップ

--- Assets/Scripts/ResultController.cs
+++ Assets/Scripts/ResultController.cs
@@ -1,16 +1,39 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class ResultController : MonoBehaviour
 {
-    [SerializeField] RectMask2D masking;
-    [SerializeField] float x;
-    [SerializeField] float y;
+    [SerializeField] RectTransform[] anchorTargets;
+    [SerializeField] RectTransform[] deltaTargets;
+    [SerializeField] float duration;
+    [SerializeField] Ease ease;
     void Start()
     {
-        var t = masking.rectTransform;
-        t.DOAnchorMax(new Vector2(x, y), 1);
-        t.DOAnchorMin(new Vector2(1 - x, 1 - y), 1);
+        foreach (var t in deltaTargets)
+        {
+            TweenDelta(t);
+        }
+
+        foreach (var t in anchorTargets)
+        {
+            TweenAnchor(t);
+        }
+    }
+
+    void TweenDelta(RectTransform t)
+    {
+        t.DOSizeDelta(t.sizeDelta, duration).SetEase(ease);
+        t.sizeDelta = Vector2.zero;
+    }
+
+    void TweenAnchor(RectTransform t)
+    {
+        t.DOAnchorMax(Vector2.one, duration).SetEase(ease);
+        t.DOAnchorMin(Vector2.zero, duration).SetEase(ease);
+        var vec = new Vector2(0.5f, 0.5f);
+        t.anchorMax = vec;
+        t.anchorMin = vec;
     }
 }
