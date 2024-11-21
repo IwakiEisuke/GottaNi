90a26bb8fcdb945cf18bc905605967e2ee6581ae
IwakiEisuke <get10.ref11.del12@gmail.com>
2024-11-21 19:30:31 +0900

    [update] #29 AddScoreSEの頭に無音部分を追加（WebGLビルドの音声ファイル圧縮の影響でノイズが発生しやすい）

--- Assets/Scripts/Game/System/ScoreManager.cs
+++ Assets/Scripts/Game/System/ScoreManager.cs
@@ -42,7 +42,12 @@ public class ScoreManager : MonoBehaviour, IGameSectionResultObserver
             scoreText.text = preText + x.ToString(format);
             if (!PinLockGameManager.GameOver && previousScore != x && soundInterval < Time.time - prevPlaySoundTime)
             {
+                //WebGLビルドの音声圧縮の影響でクリックノイズが発生するため、0.1秒スキップしたところから再生する。
+                    //短期間での連続再生時のノイズは治らなかったが0.03秒インターバルを挟んだ状態での連続再生時のノイズは低減した
+                addScoreAS.Stop(); // 再生中に timeSamples を変えてもうまく動かない
+                addScoreAS.timeSamples = 4410; // サンプル数で再生位置を指定。SEのサンプリング周波数 44100Hz / 10s => 開幕0.1秒をスキップ
                 addScoreAS.Play();
+
                 previousScore = x;
                 prevPlaySoundTime = Time.time;
             }
